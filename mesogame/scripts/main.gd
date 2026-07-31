d0adfc0eec79a972011c4d24c2425c23255853da
theflagrantwing <justintenazas@gmail.com>
2026-07-31 19:13:56 +0800

    created second grid
    added extra reference node shifted to the right, then copied over the grid generation

--- mesogame/scripts/main.gd
+++ mesogame/scripts/main.gd
@@ -15,5 +15,11 @@ func _ready() -> void:
 	for i in range(box_height):
 		for j in range(box_width):
 			var drop_instance = DROP_SPACE.instantiate()
-			drop_instance.position = $BoxTopLeftCorner.position + Vector2(i * (TILE_WIDTH + 10), j * (TILE_HEIGHT + 10))
+			drop_instance.modulate = Color(Color.MEDIUM_PURPLE, 0.7)
+			drop_instance.position = $InvTopLeftCorner.position + Vector2(i * (TILE_WIDTH + 10), j * (TILE_HEIGHT + 10))
 			add_child(drop_instance)
+	for i in range(box_height):
+		for j in range(box_width):
+			var box_instance = DROP_SPACE.instantiate()
+			box_instance.position = $BoxTopLeftCorner.position + Vector2(i * (TILE_WIDTH + 10), j * (TILE_HEIGHT + 10))
+			add_child(box_instance)
