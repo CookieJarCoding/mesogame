35e0176c57a51084acba2e0697cc4f7dfdb0ccd9
CookieJarCoding <wigginhigginliggintiggins@gmail.com>
2026-07-31 19:27:49 +0800

    quick change

--- mesogame/scripts/item_unit.gd
+++ mesogame/scripts/item_unit.gd
@@ -11,7 +11,7 @@ var initialPos: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	initialPos = global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
