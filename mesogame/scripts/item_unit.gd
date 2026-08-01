fa012ff255d2db96d32d95c15deb472843303685
kskyeddr <kskyeddr@gmail.com>
2026-08-01 10:12:54 +0800

    cleaned placement code + prevented bugs

--- mesogame/scripts/item_unit.gd
+++ mesogame/scripts/item_unit.gd
@@ -43,9 +43,8 @@ func _process(_delta: float) -> void:
 		if area.has_overlapping_areas():
 			var overlap = area.get_overlapping_areas()
 			for o in overlap:
-				if o is DropSpace:
-					if not o.occupied:
-						drop_space = o
+				if o is DropSpace and not o.occupied:
+					drop_space = o
 			body_ref = drop_space
 			is_inside_dropable = true
 			last_area_touched = area
@@ -55,34 +54,25 @@ func _process(_delta: float) -> void:
 		if area.has_overlapping_areas():
 			var overlap = area.get_overlapping_areas()
 			for o in overlap:
-					if o is DropSpace:
-						if o.occupied:
-							drop_space = null
-							body_ref = null
+				if o is DropSpace and o.occupied:
+					drop_space = null
+					body_ref = null
+		else:
+			drop_space = null
+			body_ref = null
 	
 	draggable = false
 	for area in areas:
 		area.draggable = false
-	#scale = Vector2(1, 1)
 	
 	for area in areas:
 		if area is TileCollision and area.hovered_over:
 			draggable = true
-			#scale = Vector2(1.05, 1.05)
 	
 	if draggable:
 		for area in areas:
 			area.draggable = true
 	
-	#if ($Area/Collision.global_position.x - $Area/Collision.shape.size.x/2 < get_global_mouse_position().x and get_global_mouse_position().x < $Area/Collision.global_position.x + $Area/Collision.shape.size.x/2) and ($Area/Collision.global_position.y - $Area/Collision.shape.size.y/2 < get_global_mouse_position().y and get_global_mouse_position().y < $Area/Collision.global_position.y + $Area/Collision.shape.size.y/2):
-		#if not Globals.is_dragging:
-			#draggable = true
-			#scale = Vector2(1.05, 1.05)
-	#else:
-		#if not Globals.is_dragging:
-			#draggable = false
-			#scale = Vector2(1, 1)
-	#print(get_global_mouse_position(), " ", $Area/Collision.shape.size.x, " ", $Area/Collision.global_position)
 	if draggable:
 		if Input.is_action_just_pressed("click"):
 			if body_ref:
@@ -96,7 +86,6 @@ func _process(_delta: float) -> void:
 			global_position = get_global_mouse_position() - offset
 		elif Input.is_action_just_released("click"):
 			Globals.is_dragging = false
-			#is_animating = true
 			#print("Release")
 			if is_inside_dropable and body_ref:
 				global_position = body_ref.global_position - last_area_touched.position
@@ -104,45 +93,8 @@ func _process(_delta: float) -> void:
 				global_position = initialPos
 				if prev_body_ref:
 					body_ref = prev_body_ref
-					#body_ref.occupied = true
 
 
 func _physics_process(_delta: float) -> void:
 	pass
 		# print(body_ref.position)
-
-
-#func finish_snap() -> void:
-	#is_animating = false
-
-
-"""
-func _on_area_2d_mouse_exited() -> void:
-	if not Globals.is_dragging:
-		draggable = false
-		scale = Vector2(1, 1)
-
-
-func _on_area_2d_mouse_entered() -> void:
-	if not Globals.is_dragging:
-		draggable = true
-		scale = Vector2(1.05, 1.05)
-"""
-
-
-#func _on_tile_area_entered(area: Area2D, id) -> void:
-	#if area.is_in_group("dropable") and area is DropSpace:
-		#if not area.occupied:
-			#is_inside_dropable = true
-			#area.modulate = Color(Color.REBECCA_PURPLE, 1)
-			#body_ref = area
-			#last_area_touched = id
-			#print(area.position, ": ", last_area_touched)
-#
-#
-#func _on_tile_area_exited(area: Area2D) -> void:
-	#if area.is_in_group("dropable") and area is DropSpace:
-		#is_inside_dropable = false
-		#area.modulate = Color(Color.MEDIUM_PURPLE, 0.7)
-		#prev_body_ref = body_ref
-		#body_ref = null
