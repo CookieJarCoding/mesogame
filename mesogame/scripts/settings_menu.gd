5ad36974518b6522be082991ae3364f96dff5dc8
kskyeddr <kskyeddr@gmail.com>
2026-08-02 16:13:08 +0800

    Volume sliders working

--- mesogame/scripts/settings_menu.gd
+++ mesogame/scripts/settings_menu.gd
@@ -9,13 +9,13 @@ func _ready() -> void:
 
 func _on_music_volume_slider_value_changed(value: float) -> void:
 	var bus_idx = AudioServer.get_bus_index("Music")
-	AudioServer.set_bus_volume_linear(0, value)
+	AudioServer.set_bus_volume_linear(bus_idx, value)
 	GameSettings.music_volume = value
 
 func _on_sound_volume_slider_value_changed(value: float) -> void:
 	var bus_idx = AudioServer.get_bus_index("SFX")
-	AudioServer.set_bus_volume_linear(0, value)
-	GameSettings.music_volume = value
+	AudioServer.set_bus_volume_linear(bus_idx, value)
+	GameSettings.sound_volume = value
 
 func _on_close_button_pressed() -> void:
 	self.visible = false
