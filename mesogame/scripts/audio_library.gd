e29c1b49e75cd0abc58f840d65b4ce49d3868c84
theflagrantwing <justintenazas@gmail.com>
2026-08-02 12:18:33 +0800

    added blank audiostreams

--- mesogame/scripts/audio_library.gd
+++ mesogame/scripts/audio_library.gd
@@ -2,10 +2,31 @@ extends Node
 
 
 enum sfx {
+	PAGE_FLIP,
+	PLOP_PLOP,
+	PLOP_FLAT,
+	LIQUID,
+	FRAGILE,
+	QUOTA,
+	WRONG,
 	SWOOSH
 }
 
 
 func play_sfx(s: sfx):
-	if s == sfx.SWOOSH:
+	if s == sfx.PAGE_FLIP:
+		pass
+	elif s == sfx.PLOP_PLOP:
+		pass
+	elif s == sfx.PLOP_FLAT:
+		pass
+	elif s == sfx.LIQUID:
+		pass
+	elif s == sfx.FRAGILE:
+		pass
+	elif s == sfx.QUOTA:
+		pass
+	elif s == sfx.WRONG:
+		pass
+	elif s == sfx.SWOOSH:
 		$Swoosh.play(4.0)
