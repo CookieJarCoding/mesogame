57850097e3edbb5b8a844eec8c38edc6864b8c64
CookieJarCoding <wigginhigginliggintiggins@gmail.com>
2026-08-03 14:54:26 +0800

    levels 8-10 with various fixes

---
+++ mesogame/scripts/level_scripts/level_11.gd
@@ -0,0 +1,19 @@
+extends Level
+
+func _ready() -> void:
+	super()
+	$HUD.create_character("Jay & Ethan", 0, 27,
+		[Globals.item_types.TOYS, Globals.item_types.CONSUMABLES],
+		[Globals.item_types.APPAREL],
+		"res://assets/characters/jay_ethan.png"
+	)
+	$HUD.create_character("Joanne", 0, 26,
+		[Globals.item_types.APPAREL],
+		[],
+		"res://assets/characters/joanne.png"
+	)
+	$HUD.create_character("Nanang", 0, 26,
+		[Globals.item_types.CONSUMABLES, Globals.item_types.HANDICRAFTS],
+		[Globals.item_types.TOYS],
+		"res://assets/characters/ange.png"
+	)
--- mesogame/scripts/level_scripts/level_end.gd
+++ mesogame/scripts/level_scripts/level_end.gd
@@ -4,7 +4,7 @@ extends Level
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
-	$HUD.create_character("Toni", 0, 1,
+	$HUD.create_character("You", 0, 1,
 		[],
 		[],
 		"res://assets/characters/toni_casual.png"
